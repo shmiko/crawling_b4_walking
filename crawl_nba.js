44a3901abfc82370a8a8d5135d3df0738498a73b
shmiko <shmiko@gmail.com>
2016-05-02 21:01:51 +1000

    google

--- crawl_nba.js
+++ crawl_nba.js
@@ -1,9 +1,15 @@
 var request = require("request"),
+cheerio = require("cheerio"),
 nba_url = 'http://stats.nba.com/stats/leaguedashplayerptshot?CloseDefDistRange=&College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&DribbleRange=&GameScope=&GameSegment=&GeneralRange=Overall&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2015-16&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&ShotDistRange=&StarterBench=&TeamID=0&TouchTimeRange=&VsConference=&VsDivision=&Weight=&closestDef10='
-
-// #request url and parse JSON
-
-var response = request(nba_url);
-var nba = response.json()['resultSets'][0];
-
-console.log(nba);
+var url = 'http://stats.nba.com/player/#!/201142/tracking/shots/?p=kevin-durant'
+ 
+request(url, function (error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body),
+      nba_results = $('.player').innerText;
+      
+    console.log("It’s " + nba_results + " nba score.");
+  } else {
+    console.log("We’ve encountered an error: " + error);
+  }
+});
